54aae58066be06d3dc9df786b1543c03e04158d9
LIKITH <likith.isekit2020@gmail.com>
2024-09-18 11:21:54 +0530

    HigherOrderFunction

--- HigherOrderFunction/HigherOrderFunctions.js
+++ HigherOrderFunction/HigherOrderFunctions.js
@@ -9,3 +9,14 @@
     }
 
     higherOrderFunction(greet); // "Hello!"
+
+    // Function that takes another function as an argument
+    function applyFunction(fn, value) {
+      return fn(value);
+    }
+
+    function double(x) {
+      return x * 2;
+    }
+
+    console.log(applyFunction(double, 5)); // Outputs: 10
