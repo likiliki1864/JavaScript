15a59bceb917be57f2eeb3efe31e9c7f35902e9f
LIKITH <likith.isekit2020@gmail.com>
2024-09-18 11:25:06 +0530

    Hoisting

--- Hoisting/Hoisting.js
+++ Hoisting/Hoisting.js
@@ -7,3 +7,9 @@ console.log(hoistedFunction()); // Outputs: "Hello!"
 function hoistedFunction() {
     return "Hello!";
 }
+
+console.log(a); // ReferenceError: Cannot access 'a' before initialization
+let a = 3;
+
+console.log(b); // ReferenceError: Cannot access 'b' before initialization
+const b = 5;
