9a6c55819d3ffa6ab06c91bc62cb2dc30a4b4e90
LIKITH <likith.isekit2020@gmail.com>
2024-09-18 16:11:01 +0530

    Prototypes

---
+++ Prototypes/Prototypes.js
@@ -0,0 +1,6 @@
+//Prottypes:        
+        const person1 = new Person("Likith", 25);
+        person1.sayHello(); // Output: Hello, my name is Likith
+
+        const person2 = new Person("Bharath", 30);
+        person2.sayHello(); // Output: Hello, my name is Bharath
