310f997ac42dd8c8570e4212120fcdd7cdaedf69
LIKITH <likith.isekit2020@gmail.com>
2024-09-23 03:32:40 +0530

    [LIKITH] : UC7_RefactorToClass

--- EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
@@ -1,68 +1,52 @@
-console.log("\t\t\t Welcome to EmployeeWage Computation");
-console.log("--------------------------------------------------------------------------------------------");
+class EmployeeWageComputation {
+    WagePerHour = 20;
+    FullDayHour = 8;
+    PartTimeHour = 8;
+    DailyWages = this.WagePerHour * 8;
+    Attendance = () => {
+        return Math.random() >= 0.5;
+    }
 
-const WagePerHour=20;
-const FullDayHour=8;
-const PartTimeHour=8;
+    EmployeeType = () => {
+        return Math.random() >= 0.5 ? "fullTimeEmployee" : "partTimeEmployee";
+    }
+    WagesForMonth = () => {
+        let TotalWages = 0;
+        let workingDays = 0;
+        let workingHours = 0;
 
-let DailyWages = 20 * 8;
-
-let Attendance = ()=>{if(Math.random()>=0.5){
-                        return true;
-                        }           
-                    else{
-                        return false;
-                        }
-                    }
-
-if(Attendance()){
-    console.log("\nEmployee : Present \n");
-}
-else{
-    console.log("\nEmployee : Absent \n");
-}
-
-let EmployeeType = ()=>{
-
-    if(Math.random()>=0.5)
-        return "fullTimeEmployee";         
-    else
-        return "partTimeEmployee";
-
-}
-
-let wagesForMonth = ()=>{
-
-    let TotalWages = 0;
-    let workingDays = 0;
-    let workingHours = 0;
-    /* for(let i=1; i<=20; i++){
-        if(Attendance()==true){
-            TotalWages+= DailyWages;
-        }
-    } */
-        while(workingDays <= 20 && workingHours < 100) {
+        while (workingDays <= 20 && workingHours < 100) {
             workingDays++;
-            if (Attendance()) {
+            if (this.Attendance()) {
                 workingHours += 8;
-                TotalWages += 160;
+                TotalWages += this.DailyWages;
             }
         }
+        return TotalWages;
+    }
+    Display = () => {
+        console.log("\t\t\t Welcome to Employee Wage Computation");
+        console.log("--------------------------------------------------------------------------------------------");
 
+        if (this.Attendance()) {
+            console.log("\nEmployee : Present \n");
+        } else {
+            console.log("\nEmployee : Absent \n");
+        }
 
-    return TotalWages;
-
+        const employeeType = this.EmployeeType();
+        switch (employeeType) {
+            case "fullTimeEmployee":
+                console.log("Employee Type : Full Time Employee\n");
+                console.log(`Daily Wages  : ${this.DailyWages}\n`);
+                console.log(`Monthly Wages : ${this.WagesForMonth()}`);
+                break;
+            case "partTimeEmployee":
+                console.log("Employee Type : Part Time Employee\n");
+                console.log(`Daily Wages  : ${this.DailyWages}\n`);
+                console.log(`Monthly Wages : ${this.WagesForMonth()}`);
+                break;
+        }
+    }
 }
-
-switch(EmployeeType()){
-
-    case "fullTimeEmployee" : console.log("Employee Type : Full Time Employee\n");
-                              console.log(`Daily Wages  : ${DailyWages}\n`);
-                              console.log(`Montly Wages : ${wagesForMonth()}`);
-                            break;
-    case "partTimeEmployee" : console.log("Employee Type : Part Time Employee\n");
-                              console.log(`Daily Wages  : ${DailyWages}\n`);
-                              console.log(`Montly Wages : ${wagesForMonth()}`);
-                            break;                        
-
-}
+new EmployeeWageComputation().Display();
