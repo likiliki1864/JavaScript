7a8c41a0a80d99e1d9efbeb60ee00b00bad64d49
LIKITH <likith.isekit2020@gmail.com>
2024-09-23 01:37:47 +0530

    [LIKITH] : UC2_CalculateDailyEmployeeWage

--- EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
@@ -1,5 +1,8 @@
 console.log("Welcome to EmployeeWage Computation");
 
+const WagePerHour=20;
+const FullDayHour=8;
+
 let Attendangtuce = ()=>{if(Math.random()>=0.5){
                         console.log("Employee Present");
                         return true;
@@ -12,6 +15,3 @@ let Attendangtuce = ()=>{if(Math.random()>=0.5){
 
 Attendance();
 
-
-
-    
