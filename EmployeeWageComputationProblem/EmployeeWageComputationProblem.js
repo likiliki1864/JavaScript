5212b94eab78a90a8221bf792dc763bf51105cc0
LIKITH <likith.isekit2020@gmail.com>
2024-09-23 01:41:25 +0530

    [LIKITH] : UC2_AddPartTimeEmployee&Wage

--- EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem/EmployeeWageComputationProblem.js
@@ -2,6 +2,7 @@ console.log("Welcome to EmployeeWage Computation");
 
 const WagePerHour=20;
 const FullDayHour=8;
+const PartTimeHour=8;
 
 let Attendangtuce = ()=>{if(Math.random()>=0.5){
                         console.log("Employee Present");
